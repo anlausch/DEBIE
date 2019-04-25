8b41fdb19b7d67a1e66434a13837de39248f7a11
anlausch <anne-lauscher@web.de>
2019-04-25 11:26:53 +0200

    Parameterize similarity benchmark script and include simverb

--- simlex.py
+++ simlex.py
@@ -2,6 +2,13 @@ import codecs
 import scipy.stats as stats
 import numpy as np
 import pickle
+import argparse
+
+def boolean_string(s):
+  if s not in {'False', 'True', 'false', 'true'}:
+    raise ValueError('Not a valid boolean string')
+  return s == 'True' or s == 'true'
+
 
 def mat_normalize(mat, norm_order=2, axis=1):
   return mat / np.transpose([np.linalg.norm(mat, norm_order, axis)])
@@ -14,7 +21,7 @@ def cosine(a, b):
   return cos
 
 
-def load_simlex(path="/work/anlausch/SimLex-999/SimLex-999.txt"):
+def load_benchmark(path="/work/anlausch/SimLex-999/SimLex-999.txt"):
   data = []
   with codecs.open(path, "r", "utf8") as f:
     for i, line in enumerate(f.readlines()):
@@ -36,7 +43,7 @@ def load_vectors_goran(path):
   return np.load(path)
 
 
-def run_evaluation(vocab_path, vector_path):
+def run_evaluation(vocab_path, vector_path, output_path, type="simlex"):
   """
   :param vocab_path:
   :param vector_path:
@@ -44,25 +51,44 @@ def run_evaluation(vocab_path, vector_path):
    run_evaluation("/work/gglavas/data/word_embs/yacle/fasttext/200K/npformat/ft.wiki.en.300.vocab", "/work/gglavas/data/word_embs/yacle/fasttext/200K/npformat/ft.wiki.en.300.vectors")
    >>> run_evaluation("/work/gglavas/data/word_embs/yacle/fasttext/200K/npformat/ft.wiki.en.300.vocab", "/work/anlausch/debbie/output_vectors0.0.vec")
   """
-  data = load_simlex()
-  vocab = load_vocab_goran(vocab_path)
-  vectors = load_vectors_goran(vector_path)
-  cos = cosine(vectors, vectors)
+  with codecs.open(output_path, "w", "utf8") as f:
+    if type == "simlex":
+      f.write("SimLex-999\n")
+      data = load_benchmark("/work/anlausch/SimLex-999/SimLex-999.txt")
+    elif type == "simverb":
+      f.write("SimVerb-3500\n")
+      data = load_benchmark("/home/anlausch/SimVerb-3500/SimVerb-3500.txt")
+    vocab = load_vocab_goran(vocab_path)
+    vectors = load_vectors_goran(vector_path)
+    cos = cosine(vectors, vectors)
 
-  ys = []
-  scores = []
-  for w1, w2, sim in data:
-    if w1 in vocab and w2 in vocab:
-      scores.append(cos[vocab[w1]][vocab[w2]])
-      ys.append(sim)
-    else:
-      print("not in vocab %s" % w1 + " " + w2)
+    ys = []
+    scores = []
+    for w1, w2, sim in data:
+      if w1 in vocab and w2 in vocab:
+        scores.append(cos[vocab[w1]][vocab[w2]])
+        ys.append(sim)
+      else:
+        f.write("not in vocab %s \n" % str((w1, w2)))
 
-  print("Spearman", stats.spearmanr(np.array(scores), np.array(ys)).correlation)
-  print("Pearson", stats.pearsonr(np.array(scores), np.array(ys))[0])
+    f.write("Spearman %s\n" % str(stats.spearmanr(np.array(scores), np.array(ys)).correlation))
+    f.write("Pearson %s\n" % str(stats.pearsonr(np.array(scores), np.array(ys))[0]))
+
 
 def main():
-  run_evaluation()
+  parser = argparse.ArgumentParser(description="Running Simlex + Simverb")
+  parser.add_argument("--output_path", type=str, default=None,
+                      help="Output path", required=True)
+  parser.add_argument("--embedding_vector_path", type=str, default=None,
+                      help="Embedding vector path", required=True)
+  parser.add_argument("--embedding_vocab_path", type=str, default=None,
+                      help="Embedding vocab path", required=True)
+
+  args = parser.parse_args()
+  output_path_simlex = args.output_path + "/simlex.txt"
+  output_path_simverb = args.output_path + "/simverb.txt"
+  run_evaluation(args.embedding_vocab_path, args.embedding_vector_path, output_path=output_path_simlex, type="simlex")
+  run_evaluation(args.embedding_vocab_path, args.embedding_vector_path, output_path=output_path_simverb, type="simverb")
 
 
 if __name__=="__main__":
